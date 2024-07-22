23adda20dc6f4dc3788ca53e29269ddaed53e285
paulomesquita-wsn <paulo@wellsitenavigator.com>
2024-07-22 12:09:38 -0300

    feat: add types for dashboard query

--- src/backend/application/query/GetDashboard.ts
+++ src/backend/application/query/GetDashboard.ts
@@ -1,4 +1,23 @@
+import type { Name } from "@/backend/domain/vo/EventName";
 import type { DatabaseConnection } from "@/backend/infra/database/DatabaseConnection";
+
+type PerUser = {
+  _measurement: Name;
+  count: number;
+  lastAppOpenedTime: string;
+  userId: number;
+}
+
+type PerMonth = {
+  _measurement: Name;
+  _start: string;
+  userCount: number;
+}
+
+type Output = {
+  perUser: PerUser[];
+  perMonth: PerMonth[];
+}
 
 export class GetDashboardQuery {
   constructor(private readonly influx: DatabaseConnection) {}
@@ -43,7 +62,7 @@ export class GetDashboardQuery {
     `;
   }
 
-  async execute(companyId: number, start: string, end?: string): Promise<{ perUser: any[]; perMonth: any[] }> {
+  async execute(companyId: number, start: string, end?: string): Promise<Output> {
     try {
       const startRange = start || "1970-01-01T00:00:00.000Z";
       const endRange = end || "2025-01-01T00:00:00.000Z";
@@ -51,10 +70,9 @@ export class GetDashboardQuery {
       const measurementsPerUserQuery = this.buildMeasurementsPerUserQuery(companyId, startRange, endRange);
       const usersPerMeasurementPerMonthQuery = this.buildUsersPerMeasurementPerMonthQuery(companyId, startRange, endRange);
 
-      const perUser = await this.influx.query(measurementsPerUserQuery);
-      const perMonth = await this.influx.query(usersPerMeasurementPerMonthQuery);
+      const perUser = await this.influx.query<PerUser>(measurementsPerUserQuery);
+      const perMonth = await this.influx.query<PerMonth>(usersPerMeasurementPerMonthQuery);
 
-      console.log(measurementsPerUserQuery, usersPerMeasurementPerMonthQuery)
 
       return { perUser, perMonth };
     } catch (error) {
